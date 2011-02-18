715f5eacb5ef2e2d0a254b839ad889083d553db3
Anand Kumar <anand@rinkus.com>
2011-02-18 17:02:10 -0500

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -36,6 +36,6 @@ end
 
 group :test do
   gem 'rspec', '2.0.0'
-
+  gem 'spork', '0.8.4'
 end
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
   end
 
   describe "GET 'contact'" do
@@ -15,5 +17,12 @@ describe PagesController do
       response.should be_success
     end
   end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end  
 
 end
