83f207aa5f3164196baa243f690e04a33223eda6
Anand Kumar <anand@rinkus.com>
2011-03-03 06:43:20 -0500

    static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -25,4 +25,11 @@ describe PagesController do
     end
   end  
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  end    
+
 end
