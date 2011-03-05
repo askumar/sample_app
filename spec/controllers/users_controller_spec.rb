63dcb93f89d00ec9d3cbdcdf143086eaa5734a8a
Anand Kumar <anand@rinkus.com>
2011-03-05 13:59:41 -0500

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,6 +14,27 @@ describe UsersController do
     #           response.should have_selector("title", :content => "Sign up")
     #     end
     
+    it "should have a name field" do
+          get :new
+          response.should have_selector("input[name='user[name]'][type='text']")
+    end
+    
+    it "should have a email field" do
+          get :new
+          response.should have_selector("input[name='user[email]'][type='text']")
+    end
+    
+    it "should have a password field" do
+          get :new
+          response.should have_selector("input[name='user[password]'][type='password']")
+    end
+    
+    it "should have a password confirmation field" do
+          get :new
+          response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
+    
+        
 
   end
   
